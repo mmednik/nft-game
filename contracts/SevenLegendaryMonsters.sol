4ea7b08166a0335533392c910a5e606239b6184f
mmednik <mmednik@gmail.com>
2022-02-25 20:46:15 -0300

    base hardhat files

---
+++ contracts/SevenLegendaryMonsters.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+
+contract SevenLegendaryMonsters {
+  constructor() {
+    console.log("THIS IS MY GAME CONTRACT. NICE.");
+  }
+}
