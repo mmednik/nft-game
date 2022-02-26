616aab6457fb188341b7dbd70cd648e3bdd294a8
mmednik <mmednik@gmail.com>
2022-02-25 21:13:58 -0300

    wip - characters attributes

--- contracts/SevenLegendaryMonsters.sol
+++ contracts/SevenLegendaryMonsters.sol
@@ -1,9 +1,52 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract SevenLegendaryMonsters {
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT. NICE.");
-  }
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+        uint256 defense;
+        uint256 criticChance;
+    }
+
+    CharacterAttributes[] defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg,
+        uint256[] memory characterDefense,
+        uint256[] memory characterCriticChance
+    ) {
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i],
+                    defense: characterDefense[i],
+                    criticChance: characterCriticChance[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s, def %s, crit %s",
+                c.name,
+                c.hp,
+                c.imageURI,
+                c.defense,
+                c.criticChance
+            );
+        }
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,31 @@
 const main = async () => {
-  const gameContractFactory = await hre.ethers.getContractFactory('SevenLegendaryMonsters');
-  const gameContract = await gameContractFactory.deploy();
+  const gameContractFactory = await hre.ethers.getContractFactory(
+    "SevenLegendaryMonsters"
+  );
+  const gameContract = await gameContractFactory.deploy(
+    // Names
+    [
+      "Teyú Yaguá",
+      "Mbói Tu'i",
+      "Moñái",
+      "Yasy Yateré",
+      "Kurupí",
+      "Ao Ao",
+      "Lobisón",
+    ], 
+
+    // Images
+    [
+      "https://i.imgur.com/pKd5Sdk.png",
+      "https://i.imgur.com/xVu4vFL.png",
+      "https://i.imgur.com/WMB6g9u.png",
+    ],
+
+    [100, 200, 300], // HP values
+    [100, 50, 25], // Attack damage values
+    [100, 50, 25], // Defense values
+    [100, 50, 25] // Critical chance values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
